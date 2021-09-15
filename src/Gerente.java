ed6b340ea9ea5225de31979d3302fd4e6f8790bb
Lucas Mateus Midao Fernandes <mateusmidao@gmail.com>
2021-09-15 19:13:54 -0300

    Começando com a herança

---
+++ src/Gerente.java
@@ -0,0 +1,36 @@
+
+// Gerente é um Funcionário, Gerente herda da classe Funcionário
+// Assim podemos corrigir o problema da aula (commit) anterior.
+public class Gerente extends Funcionario {
+	
+	private int senha;
+	
+	// Construtores
+	public Gerente() {
+		
+	}
+	
+	// Métodos da classe
+//	public double getBonificacao() {
+//		return this.salario;
+//	};
+	
+	public boolean autentica(int senha) {
+		if (this.senha == senha) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	// Métodos getters
+	public int getSenha() {
+		return this.senha;
+	}
+	
+	// Métodos setters
+	public void setSenha(int senha) {
+		this.senha = senha;
+	}
+	
+}
