5f467085dd2219f2811397bb3c719c92d1899688
Lucas Mateus Midao Fernandes <mateusmidao@gmail.com>
2021-09-16 16:23:31 -0300

    Resumo Herança

--- src/TesteReferencias.java
+++ src/TesteReferencias.java
@@ -36,10 +36,14 @@ public class TesteReferencias {
 		EditorVideo ev = new EditorVideo();
 		ev.setSalario(2500.00);
 		
+		Designer d = new Designer();
+		d.setSalario(2000.00);
+		
 		ControleBonificacao controle = new ControleBonificacao();
 		controle.registra(g1);
 		controle.registra(f);
 		controle.registra(ev);
+		controle.registra(d);
 		
 		System.out.println(controle.getSoma());
 	}
