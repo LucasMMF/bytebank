7864ef6f7cecc4f85ce88ea37d4481512c91bf67
Lucas Mateus Midao Fernandes <mateusmidao@gmail.com>
2021-09-16 20:01:42 -0300

    Conta Corrente e Poupança

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -5,4 +5,9 @@ public class ContaCorrente extends Conta {
 		super(agencia, numero);
 	}
 	
+	@Override
+	public boolean saca(double valor) {
+		valor += 0.2;
+		return super.saca(valor);
+	}
 }
--- src/TesteContas.java
+++ src/TesteContas.java
@@ -14,7 +14,18 @@ public class TesteContas {
 		System.out.println(conta.getAgencia());
 		
 		System.out.println("O total de contas é: " + Conta.getTotal());
-
+		
+		ContaCorrente cc = new ContaCorrente(1111, 1111);
+		cc.deposita(100.00);
+		
+		ContaPoupanca cp = new ContaPoupanca(2222, 2222);
+		cp.deposita(200.00);
+		
+		
+		cc.transfere(10.00, cp);
+		
+		System.out.println("CC: " + cc.getSaldo());
+		System.out.println("CP: " + cp.getSaldo());
 	}
 
 }
