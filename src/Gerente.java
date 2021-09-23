52ff2ae677d995c4df8ee5bacab4fbf7297a9d08
Lucas Mateus Midao Fernandes <mateusmidao@gmail.com>
2021-09-23 16:30:19 -0300

    Herança multipla?

--- src/Gerente.java
+++ src/Gerente.java
@@ -1,7 +1,7 @@
 
 // Gerente é um FuncionárioAutenticavel, Gerente herda da classe FuncionárioAutenticavel
 // Assim podemos corrigir o problema da aula (commit) anterior.
-public class Gerente extends FuncionarioAutenticavel {
+public class Gerente extends Funcionario {
 	
 	// Construtores
 	public Gerente() {
